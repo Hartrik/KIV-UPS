97118002be6c2278cda6bdc161209200634b949b
Patrik Harag <patrikharag@gmail.com>
2017-11-12 13:05:14 +0100

    [Client] Use BufferedReader

--- client/src/main/java/cz/hartrik/puzzle/net/ReaderThread.java
+++ client/src/main/java/cz/hartrik/puzzle/net/ReaderThread.java
@@ -1,5 +1,6 @@
 package cz.hartrik.puzzle.net;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -32,7 +33,8 @@ public class ReaderThread extends Thread {
         if (reader != null)
             throw new IllegalStateException("Stream already initialized");
 
-        this.reader = new InputStreamReader(inputStream, charset);
+        InputStreamReader streamReader = new InputStreamReader(inputStream, charset);
+        this.reader = new BufferedReader(streamReader);
     }
 
     @Override
